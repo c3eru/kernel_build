36ed24564fedd8a270c9ef2a3f28e64c71d73d56
c3eru <yyprjkt@gmail.com>
2022-07-17 11:10:12 +0700

    cleanup

--- sync.sh
+++ sync.sh
@@ -8,5 +8,5 @@ git clone --depth=1 $GH_TOKEN@github.com/c3eru/kernel -b MIUI-CITRUS-stg $CIRRUS
 git clone --depth=1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/PROTON-CLANG
 # Azure Clang ---
 git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $CIRRUS_WORKING_DIR/AZURE-CLANG
-# Greenforce Clang ---
-git clone --depth=1 https://github.com/greenforce-project/clang-llvm $CIRRUS_WORKING_DIR/GF-CLANG
+# xRageTC Clang ---
+git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang $CIRRUS_WORKING_DIR/XRAGE-CLANG
