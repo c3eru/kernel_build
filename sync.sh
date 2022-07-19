dad1810f3430155ca42ce9f718ae4ca1db1367c1
c3eru <yyprjkt@gmail.com>
2022-07-19 12:25:50 +0700

    cleanup

--- sync.sh
+++ sync.sh
@@ -13,4 +13,6 @@ git clone --depth=1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/
 git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $CIRRUS_WORKING_DIR/AZURE-CLANG
 # Xrage Clang ---
 git clone --depth=1 https://github.com/xyz-prjkt/xRageTC-clang $CIRRUS_WORKING_DIR/XRAGE-CLANG
+# Greenforce Clang ---
+git clone --depth=1 https://github.com/greenforce-project/clang-llvm $CIRRUS_WORKING_DIR/GF-CLANG
 	
