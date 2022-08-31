b48fc99e26a4e3507b7e6a934ffddb189672ab89
c3eru <96016081+c3eru@users.noreply.github.com>
2022-08-31 07:52:25 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ git clone --depth=1 $GH_TOKEN@github.com/c3eru/$SOURCE_GH -b MIUI-CITRUS $CIRRUS
 
 # Tool Chain
 # Proton Clang ---
- git clone --depth=1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/PROTON-CLANG
+ git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang/-/tree/clang-15 $CIRRUS_WORKING_DIR/PROTON-CLANG
 # Azure Clang ---
  git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $CIRRUS_WORKING_DIR/AZURE-CLANG
 # Xrage Clang ---
