a5b3d00f2a9e8f7705169c7224480e43a2cac9fe
c3eru <96016081+c3eru@users.noreply.github.com>
2022-10-20 04:31:56 +0000

    Test

--- sync.sh
+++ sync.sh
@@ -9,6 +9,8 @@ git clone --depth=1 https://github.com/c3eru/A10 -b main $CIRRUS_WORKING_DIR/KER
 # Tool Chain
 # Proton Clang ---
  git clone --depth=1 https://github.com/kdrag0n/proton-clang $CIRRUS_WORKING_DIR/PROTON-CLANG
+ git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 $CIRRUS_WORKING_DIR/GCC
+ git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 $CIRRUS_WORKING_DIR/GCC32
 # Azure Clang ---
 # git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $CIRRUS_WORKING_DIR/AZURE-CLANG
 # Xrage Clang ---
